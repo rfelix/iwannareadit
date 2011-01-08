7427c9f9c917f8da6a0785ff170a7366ebab61b9
Raoul Felix <rf@rfelix.com>
2011-01-08 18:55:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IwannareaditRmu4::Application.config.secret_token = 'bae5d0d1d70f93a5c65ac104e7dace57eed307e590f18234ca6a1e1839aa45ce1220acb6e19b6c3ae0040bfc1614e17cf6c9a156d3d86eacc577756fb3ebd5ef'

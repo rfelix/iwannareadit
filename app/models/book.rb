f7ff7a8ee25a83862af7013031bacbbaedbe12d0
Raoul Felix <rf@rfelix.com>
2011-01-27 15:30:36 +0000

    Changed method alias in Book to use an association with order
    Also added additional validation to :direction of Vote
    to be only integers

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,15 +1,10 @@
 class Book < ActiveRecord::Base
   has_many :votes
-  has_many :comments
+  has_many :comments, :order => "comments.created_at desc"
 
   validates :name, :presence => true
   validates :authors, :presence => true
 
   scope :not_bought, where(:is_bought => false)
   scope :bought, where(:is_bought => true)
-
-  alias_method :_original_comments, :comments
-  def comments
-    _original_comments.reverse
-  end
 end
